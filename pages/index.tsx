a9f290a3df89f10128d10e9eb07136c937570cd0
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-09-06 20:14:50 +0100

    added skill section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import NavBar from '../components/NavBar';
 import Skills from '../components/skill/index';
 import Actions from '../components/actions/Actions';
-import Action from '../components/actions/Action';
 
 const Home: NextPage = () => {
   return (
@@ -19,7 +18,9 @@ const Home: NextPage = () => {
         <section className="h-screen text-white w-full mx-10 md:mx-20 2xl:mx-36 flex flex-col justify-center gap-y-10">
           <div className="">
             <p className="text-[80px] font-extrabold">Connecting Competent</p>
-            <p className="text-[80px] font-extrabold">tech and web3 talents</p>
+            <p className="text-[80px] font-extrabold">
+              tech and web3 <span className="text-[#00A3FF]">talents</span>
+            </p>
             <p className="text-6xl font-normal">
               to the best recruiter in reels.
             </p>
@@ -57,7 +58,6 @@ const Home: NextPage = () => {
       </section>
 
       <Actions />
-    
 
       <section className="bg-[#101010] md:border-solid border-y-2 md:border-gray-500/50 ">
         <div className="grid grid-cols-1 pl-10 md:pl-0 md:grid-cols-2 items-center 2xl:mx-60 2xl:my-16">
@@ -73,7 +73,7 @@ const Home: NextPage = () => {
             </p>
             <a
               href="#"
-              className="py-5 px-10 rounded-full flex gap-x-3 items-center"
+              className="py-5 px-0 rounded-full flex gap-x-3 items-center"
             >
               <p className="text-white text-base font-extrabold">Know more</p>{' '}
               <svg
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
                 viewBox="0 0 24 24"
                 stroke-width="1.5"
                 stroke="currentColor"
-                className="w-5 h-5"
+                className="w-5 h-5 text-white"
               >
                 <path
                   stroke-linecap="round"
@@ -95,22 +95,61 @@ const Home: NextPage = () => {
 
           <div className="flex flex-col md:border-solid md:border-l-2 md:border-gray-500/50">
             <div className="flex gap-4 pl-10 text-white md:border-solid md:border-b-2 md:border-gray-500/50 max-w-[624px] pb-10">
-              <div>hr</div>
               <div>
-                <h4>Industries we serve</h4>
-                <p>Tech, Blockchain, Fintech, E-Commerce Web2 and Web3</p>
+                <Image
+                  height={60}
+                  width={60}
+                  className="h-8 w-8"
+                  src="/industries.svg"
+                  alt="icon"
+                />
+              </div>
+              <div>
+                <h4 className="font-bold text-xl">Industries we serve</h4>
+                <p className="text-base text-[#8F8F8F]">
+                  Tech, Blockchain, Fintech, E-Commerce Web2 and Web3
+                </p>
               </div>
             </div>
             <div className="flex gap-4 pl-10 text-white pt-10">
-              <div>hr</div>
               <div>
-                <h4>Who can join?</h4>
-                <p>Competent Web3/tech talents/creatives and top recruiters</p>
+                <Image
+                  height={60}
+                  width={60}
+                  className="h-8 w-8"
+                  src="/join.svg"
+                  alt="iccon"
+                />
+              </div>
+              <div>
+                <h4 className="font-bold text-xl">Who can join?</h4>
+                <p className="text-base text-[#8F8F8F]">
+                  Competent Web3/tech talents/creatives and top recruiters
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div>{/* <Actions/> */}</div>
+      </section>
+
+      <section>
+        <div className="grid grid-cols-1 md:grid-cols-2 items-center h-['3000px']">
+          <div className="text-white">
+            <h3>Checkout Our Top</h3>
+            <h3>Skills ad Hire!</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis
+              aute irure dolor in reprehenderit in voluptate velit esse cillum
+              dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+              non proident, sunt in culpa qui officia deserunt mollit anim id
+              est laborum."
+            </p>
+          </div>
+          <div className=" hidden md:block bg-[url('/Frame.svg')] bg-no-repeat bg-cover">
+            
+          </div>
+        </div>
       </section>
 
       <footer className="">
