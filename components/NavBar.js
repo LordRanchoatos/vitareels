71ac917ac8a647a3a60dcfea2741766db32f9c19
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-09-09 14:23:15 +0100

    nav responsive

--- components/NavBar.js
+++ components/NavBar.js
@@ -75,7 +75,7 @@ function Nav() {
               <button
                 onClick={() => setIsOpen(!isOpen)}
                 type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-black"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
               >
@@ -156,6 +156,18 @@ function Nav() {
                   className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
                   HIRE A TALENT
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white flex items-center gap-2 px-3 py-2 rounded-md text-base font-medium"
+                >
+                  <FaTwitter /> TWITTER
+                </a>
+                <a
+                  href="#"
+                  className="text-gray-300 hover:text-white flex items-center gap-2 px-3 py-2 rounded-md text-base font-medium"
+                >
+                  <BsDiscord /> DISCORD
                 </a>
               </div>
             </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ const Home: NextPage = () => {
     <div className="bg-black">
       <Head>
         <title>VitaReels</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
+        <meta name="description" content="connecting web3 talents" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -58,7 +60,7 @@ const Home: NextPage = () => {
         <Skills />
       </section>
 
-        <Carousel />
+      <Carousel />
 
       <Actions />
 
