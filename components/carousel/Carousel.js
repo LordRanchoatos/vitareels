be1d57468a39a7edf165f275425b81e096da679e
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-09-11 19:22:59 +0100

    other carousel images fixed

--- components/carousel/Carousel.js
+++ components/carousel/Carousel.js
@@ -4,8 +4,8 @@ import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
 
 const images = [
   '/caro1.png',
-  '/caro1.png',
-  '/caro1.png',
+  '/caro2.png',
+  '/caro3.png',
   '/caro1.png',
 ];
 
