cffd2c04b924f1db2203de9d76959f22d8660cf8
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-09-13 11:38:37 +0100

    added the font to body

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <body className='ont-telegraf'>
+      <body className='font-telegraf'>
         <Main />
         <NextScript />
       </body>
