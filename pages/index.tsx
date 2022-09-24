8a96b7426b1ebef298f95e7015d82c40d7adb761
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-09-24 23:23:01 +0100

    fixed next metadata bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
         />
-        <meta name="description" content="connecting web3 talents" />
+        {/* <meta name="description" content="connecting web3 talents" /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
